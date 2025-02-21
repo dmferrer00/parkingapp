1a10945f60500324c54c1a089873a0c809112562
Daniel Ferrer <dferrer@uncc.edu>
2025-02-21 01:37:24 -0500

    Fixed app file

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask 
-app = Flask(name) 
-@app.route("/") 
-def hello(): 
-    return "Hello, Azure VM."
+from flask import Flask  
+
+app = Flask(__name__)  
+
+@app.route("/")  
+def hello():  
+    return "Hello, Azure VM."  
+
+if __name__ == "__main__":  
+    app.run(host="0.0.0.0", port=8000)  # Important for Azure deployment  
