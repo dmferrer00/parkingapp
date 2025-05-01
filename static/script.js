8ba55018b96398b5eb53581175d6a170275c69f7
JT Madden <143136330+JTM-UNCC@users.noreply.github.com>
2025-05-01 19:50:19 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,7 +8,6 @@ async function predict(location) {
   const data = await response.json();
 
   document.getElementById("result").innerHTML = `
-    <h3>Prediction for ${data.location}</h3>
     <p><strong>Day:</strong> ${data.day}</p>
     <p><strong>Time Block:</strong> ${data.time}</p>
     <p><strong>Availability:</strong> ${data.availability} (${data.confidence}% confident)</p>
